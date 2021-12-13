067005563edd6a0190b9af35fc405fd54de84e92
jdmsharpe <jdmsharpe@gmail.com>
2021-12-12 21:50:28 -0800

    clang

--- main.cpp
+++ main.cpp
@@ -3,31 +3,30 @@
 
 #include "Window.h"
 
-int main(int argc, char* argv[]) {
-	// Construct SDL window
-	std::unique_ptr<Window> window = std::make_unique<Window>();
-	window->Open();
+int main(int argc, char *argv[]) {
+  // Construct SDL window
+  std::unique_ptr<Window> window = std::make_unique<Window>();
+  window->Open();
 
+  // Main loop flag
+  bool quit = false;
+  // Event handler
+  SDL_Event e;
 
-	// Main loop flag
-	bool quit = false;
-	// Event handler
-	SDL_Event e;
+  while (!quit) {
+    // Handle queue
+    while (SDL_PollEvent(&e) != 0) {
+      // User closes window
+      if (e.type == SDL_QUIT) {
+        quit = true;
+      }
+    }
 
-	while (!quit) {
-		// Handle queue
-		while (SDL_PollEvent(&e) != 0) {
-			// User closes window
-			if (e.type == SDL_QUIT) {
-				quit = true;
-            }
-		}
+    // Update window
+    window->Render();
+  }
 
-		// Update window
-		window->Render();
-	}
+  window->Close();
 
-	window->Close();
-
-	return 0;
+  return 0;
 }
