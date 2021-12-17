99116503b1d98cab25621ae9fd30d94fb686b449
vligai <vlad.ligai2@gmail.com>
2021-12-17 08:18:29 -0800

    fix movement

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -7,87 +7,104 @@ constexpr double k_baseVel = 0.1;
 const std::string k_filename = "../memeinvaders/assets/enemy1.png";
 constexpr int k_projectileHeightLimit = -k_height;
 constexpr int k_maxProjectiles = 2;
-constexpr int k_shotTimeout = 1000; // ms
+constexpr int k_shotTimeout = 1000;         // ms
 } // namespace
 
 Enemy::Enemy(SDL_Renderer *renderer) : DynamicEntity(renderer, "enemy") {
-  // Enemy should spawn in bottom-middle of screen
-  SetPosition(SCREEN_WIDTH / 2, 0);
-  SetVelocity(0.0, 0.0);
+        // Enemy should spawn in bottom-middle of screen
+        SetPosition(SCREEN_WIDTH / 2, 0);
+        SetVelocity(0.0, 0.0);
 
-  m_screenBox = new SDL_Rect{0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
-  m_textureBox = new SDL_Rect{m_xPos, m_yPos, k_width, k_height};
+        m_screenBox = new SDL_Rect {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
+        m_textureBox = new SDL_Rect {m_xPos, m_yPos, k_width, k_height};
 }
 
-Enemy::~Enemy() {}
+Enemy::~Enemy() {
+}
 
 bool Enemy::Initialize() {
-  // Something really went wrong
-  if (m_renderer == NULL) {
-    std::cout << "Enemy pointer to renderer was NULL!" << std::endl;
-    return false;
-  }
+        // Something really went wrong
+        if (m_renderer == NULL) {
+                std::cout << "Enemy pointer to renderer was NULL!" << std::endl;
+                return false;
+        }
 
-  if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
-      std::cout << "SDL_Image could not initialize!" << std::endl;
-      return false;
-  }
+        if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+                std::cout << "SDL_Image could not initialize!" << std::endl;
+                return false;
+        }
 
-  // Load image and create texture
-  SDL_Surface *surface = IMG_Load(k_filename.c_str());
-  if (surface == NULL) {
-    std::cout << "Unable to load image " << k_filename
-              << "! SDL Error: " << SDL_GetError() << std::endl;
-    return false;
-  }
-  m_texture = SDL_CreateTextureFromSurface(m_renderer, surface);
-  SDL_FreeSurface(surface);
-  return true;
+        // Load image and create texture
+        SDL_Surface *surface = IMG_Load(k_filename.c_str());
+        if (surface == NULL) {
+                std::cout << "Unable to load image " << k_filename
+                          << "! SDL Error: " << SDL_GetError() << std::endl;
+                return false;
+        }
+        m_texture = SDL_CreateTextureFromSurface(m_renderer, surface);
+        SDL_FreeSurface(surface);
+        return true;
 }
 
 void Enemy::Render() {
-  SDL_RenderCopy(m_renderer, m_texture, &*m_screenBox, &*m_textureBox);
-  // Surely there must be an easier way to do this
-  m_textureBox->x = m_xPos;
-  m_textureBox->y = m_yPos;
+        SDL_RenderCopy(m_renderer, m_texture, &*m_screenBox, &*m_textureBox);
+        // Surely there must be an easier way to do this
+        m_textureBox->x = m_xPos;
+        m_textureBox->y = m_yPos;
 
-  // Take care of rendering and destroying shots
-  // for (int i = 0; i < m_shotsPresent; ++i) {
-  //   // If projectile is at top of screen, delete it and decrement shots present
-  //   if (m_projectileArray[i]->GetPosition().second < k_projectileHeightLimit) {
-  //     m_projectileArray.erase(m_projectileArray.begin() + i);
-  //     m_shotsPresent--;
-  //   } else {
-  //     m_projectileArray[i]->Render();
-  //   }
-  // }
+        // Take care of rendering and destroying shots
+        // for (int i = 0; i < m_shotsPresent; ++i) {
+        //   // If projectile is at top of screen, delete it and decrement shots present
+        //   if (m_projectileArray[i]->GetPosition().second < k_projectileHeightLimit) {
+        //     m_projectileArray.erase(m_projectileArray.begin() + i);
+        //     m_shotsPresent--;
+        //   } else {
+        //     m_projectileArray[i]->Render();
+        //   }
+        // }
 }
-
+bool hit_left = false;
+bool hit_right = true;
 void Enemy::Move() {
-  // Handle all directions and bounds check
-  // (rule is don't let sprite go off screen)
-  double xVelSum = 0.0;
-  double yVelSum = 0.0;
-  bool hit = false;
+        // Handle all directions and bounds check
+        // (rule is don't let sprite go off screen)
+        double xVelSum = 0.0;
+        double yVelSum = 0.0;
+        // bool hit_left = false;
+        // bool hit_right = false;
+        //THIS NEEDS OPTIMIZATION
+        if (m_xPos > 0) {
+                if (hit_right == true) {
+                        //Hit left wall
+                        xVelSum -= k_baseVel;
+                        if (m_xPos == 1) {
+                                hit_left = true;
+                                hit_right = false;
+                        }
+                }
 
+        }
 
-  if (m_xPos > 0 && hit == true) {
-    hit = true;
-    xVelSum -= k_baseVel;
-  } else if (m_xPos < SCREEN_WIDTH && hit == false) {
-    //hit = false;
-    xVelSum += k_baseVel;
-  }
+        if (m_xPos < SCREEN_WIDTH - k_width) {
+                //hit left wall
+                if (hit_left == true) {
+                        xVelSum += k_baseVel;
+                        if (m_xPos == SCREEN_WIDTH - k_width - 1) {
+                                hit_left = false;
+                                hit_right = true;
+                        }
+                }
+        }
 
-  // if (keyboardState[SDL_SCANCODE_UP] && m_yPos > 0) {
-  //   yVelSum -= k_baseVel;
-  // }
-  // if (keyboardState[SDL_SCANCODE_DOWN] &&
-  //     m_yPos < SCREEN_HEIGHT - k_height) {
-  //   yVelSum += k_baseVel;
-  // }
-  SetVelocity(xVelSum, yVelSum);
-  UpdatePositionFromVelocity();
+        // if (keyboardState[SDL_SCANCODE_UP] && m_yPos > 0) {
+        //   yVelSum -= k_baseVel;
+        // }
+        // if (keyboardState[SDL_SCANCODE_DOWN] &&
+        //     m_yPos < SCREEN_HEIGHT - k_height) {
+        //   yVelSum += k_baseVel;
+        // }
+        SetVelocity(xVelSum, yVelSum);
+        UpdatePositionFromVelocity();
 }
 
 // void Enemy::Fire() {
