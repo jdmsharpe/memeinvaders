80d4cba8636347e9ab6ac82a6acbf0177bedd91d
jdmsharpe <jmagnuss@andrew.cmu.edu>
2022-01-07 20:24:35 -0800

    Refactor Initialize()

--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -20,30 +20,7 @@ MainMenu::MainMenu(SDL_Renderer *renderer)
 MainMenu::~MainMenu() {}
 
 bool MainMenu::Initialize() {
-  // Something really went wrong
-  if (m_renderer == NULL) {
-    SDL_LogError(SDL_LOG_CATEGORY_ERROR,
-                 "MainMenu pointer to renderer was NULL!");
-    return false;
-  }
-
-  if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
-    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL_Image could not initialize!");
-    return false;
-  }
-
-  // Load image and create texture
-  SDL_Surface *surface = IMG_Load(k_filename.c_str());
-  if (surface == NULL) {
-    SDL_LogError(SDL_LOG_CATEGORY_ERROR,
-                 "Unable to load image %s! Error code: %s.", k_filename.c_str(),
-                 SDL_GetError());
-    return false;
-  }
-
-  m_texture = SDL_CreateTextureFromSurface(m_renderer, surface);
-  SDL_FreeSurface(surface);
-  return true;
+  return (ImgInit() && CreateTexture(k_filename));
 }
 
 void MainMenu::Render() {
