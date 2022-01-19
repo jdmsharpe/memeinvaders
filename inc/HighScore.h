3cc2e2c4c5cd5c48d2ec505ebf5d2838270f507b
jdmsharpe <jmagnuss@andrew.cmu.edu>
2022-01-18 21:51:55 -0800

    Scores display in an acceptable way...

--- inc/HighScore.h
+++ inc/HighScore.h
@@ -24,10 +24,16 @@ public:
   bool AddEntry(const ScoreEntry& entry);
 
 private:
-  int m_scoreHeight = -1;
-  SDL_Rect *m_textBox = NULL;
-  SDL_Surface *m_textSurface = NULL;
-  SDL_Texture *m_textTexture = NULL;
+  int m_numScores = -1;
+  SDL_Rect *m_nameBox = NULL;
+  SDL_Rect *m_scoreBox = NULL;
+  SDL_Rect *m_highScoreBox = NULL;
+  SDL_Surface *m_nameSurface = NULL;
+  SDL_Surface *m_scoreSurface = NULL;
+  SDL_Surface *m_highScoreSurface = NULL;
+  SDL_Texture *m_nameTexture = NULL;
+  SDL_Texture *m_scoreTexture = NULL;
+  SDL_Texture *m_highScoreTexture = NULL;
 
   std::vector<ScoreEntry> m_highScores;
 
